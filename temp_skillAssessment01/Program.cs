721bafb7f1f63c5e51ecd38d4441797de7f78637
Yahya <yahyasiddiqui885@gmail.com>
2023-05-20 14:45:57 +0500

    Add project files.

---
+++ temp_skillAssessment01/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace temp_skillAssessment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string string_ = "Hello World";
+            string resultantString=ReversedString(string_);
+            Console.WriteLine(resultantString);
+          
+            Console.ReadKey();
+        }
+        static char[] stringToArray(string string_)
+
+        {
+            char[] arr = new char[string_.Length];
+            for (int i = 0; i < string_.Length; i++)
+            {
+                arr[i] = string_[i];
+
+            }
+
+            return arr;
+        }
+        static string ReversedString( string string_)
+        {
+            char[] arr= new char[string_.Length];
+            arr= stringToArray(string_);
+            string resultant ="";
+
+            for (int i = arr.Length-1; i >= 0; i--)
+            {
+                resultant+=arr[i]; 
+            }
+
+
+            return resultant;
+        }
+
+    }
+}
